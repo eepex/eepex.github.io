013718e73c71d3547233c0eef5970ec10c6fe0d0
Sid Ghimire <siddharthaghimire@gmail.com>
2022-08-09 22:57:28 +0545

    Fixed Everything

--- index.php
+++ index.php
@@ -33,73 +33,8 @@
     <script src="./script/script.js"></script>
 
 
-    <div class="d-flex sticky-top"
-        style="padding-top:10px;padding-right: 40px;border-bottom: solid 1px rgb(216, 216, 216);background-color: white;">
-        <div style="flex: 0.7;">
-            <img src="./UI UX/Logo.png" class="title-image" />
-        </div>
+    <?php include("./src/header.html") ?>
 
-        <div class="row" style="flex:1;padding-top:10px ;">
-            <div class="title-tab-div col-2 d-none d-lg-block">
-                <a class="title-link" href="#bottom">
-                    <div class="title-tabs">
-                        <span class="title-link">
-                            Home
-                        </span>
-                    </div>
-                </a>
-            </div>
-            <div class="title-tab-div col-2 d-none d-lg-block">
-                <a class="title-link" href="#bottom">
-                    <div class="title-tabs">
-                        <span class="title-link">
-                            Contact
-                        </span>
-                    </div>
-                </a>
-            </div>
-            <div class="title-tab-div col-2 d-none d-lg-block">
-                <a class="title-link" href="#bottom">
-                    <div class="title-tabs">
-                        <span class="title-link">
-                            Events
-                        </span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="title-tab-div col-2 d-none d-lg-block">
-                <a class="title-link" href="#bottom">
-                    <div class="title-tabs">
-                        <span class="title-link">
-                            Notices
-                        </span>
-                    </div>
-                </a>
-            </div>
-
-            <div class="title-tab-div col-2 d-none d-lg-block">
-
-                <div class="title-tabs-order">
-                    <i class="fa fa-info"
-                        style="padding: 5px;background-color:white;border-radius:100%;width:22px;color:black;margin-right:10px"></i>Sponsors
-                </div>
-            </div>
-        </div>
-        <div class="dropdown d-block d-lg-none">
-            <button style="border: none;background-color:#fff" class="dropright" type="button" id="dropdownMenuButton"
-                data-toggle="dropdown">
-                <i class="fa fa-bars "
-                    style="background-color: #FF6600;color:white;padding: 10px;margin-top: 10px;"></i>
-            </button>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Home</a>
-                <a class="dropdown-item" href="#">About</a>
-                <a class="dropdown-item" href="#">Contact</a>
-            </div>
-        </div>
-
-    </div>
 
     <div class="swiper">
         <!-- Additional required wrapper -->
@@ -288,64 +223,8 @@
         </div>
     </div>
 
+    <?php include("./src/footer.html") ?>
 
-    <div class="footer-container row g-0">
-        <div class="footer-section-1 d-flex flex-column col col-md-12 col-lg-4" >
-            <div class="app-footer-title">EEPEx 2022</div>
-            <div class="app-footer-desc">EEPEx 2022 is the biggest electrical and electronics project of 2022. Hosted by
-                Society of Electrical and Electronics Engineering of Kathmandu University. This event will provide you
-                the biggest experience of your lifetime.
-            </div>
-            <div class="fa-footer-div" style="margin-top:20px">
-                <i class="fa fa-facebook fa-footer-icon" style="background-color: blue"></i>
-                <i class="fa fa-instagram fa-footer-icon" style="background-color: #E12F6B"></i>
-                <i class="fa fa-twitter fa-footer-icon" style="background-color: #1D9BF0;"></i>
-                <i class="fa fa-github fa-footer-icon" style="background-color: black"></i>
-                <i class="fa fa-linkedin fa-footer-icon" style="background-color: #0E76A8;"></i>
-            </div>
-        </div>
-        <div class="footer-section-2 col-lg-4">
-            <div class="useful-links">Useful Links</div>
-            <div class="d-flex row">
-                <div class="col-6" style="display: flex;flex-direction:column">
-                    <div class="links-list-item">Home</div>
-                    <div class="links-list-item">FAQ</div>
-                    <div class="links-list-item">Events</div>
-                    <div class="links-list-item">Bug Report</div>
-                </div>
-                <div class="col-6" style="display: flex;flex-direction:column">
-                    <div class="links-list-item">Notices</div>
-                    <div class="links-list-item">Organizers</div>
-                    <div class="links-list-item">Competition</div>
-                    <div class="links-list-item">Code Of Conduct</div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-section-3 col-md-12 col-lg-3">
-            <div class="useful-links">Contact Us</div>
-            <div class="">
-                <div class="">
-                    <div class="links-list-item">
-                        <i class="fa fa-map-pin contact-us-icon"></i>
-                        Dhulikhel, Kavre
-                    </div>
-                    <div class="links-list-item">
-                        <i class="fa fa-phone contact-us-icon"></i>
-                        9844442363
-                    </div>
-                    <div class="links-list-item">
-                        <i class="fa fa-envelope contact-us-icon"></i>
-                        siddharthaghimire@gmail.com
-                    </div>
-                    <div class="links-list-item d-flex" style="margin-top: 20px;">
-                        <i class="fa fa-paypal contact-us-icon"></i>
-                        <i class="fa fa-amazon contact-us-icon"></i>
-                        <i class="fa fa-google contact-us-icon"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div id="bottom"></div>
 
